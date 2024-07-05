df7a52ccc4c893a6eac5d138e0b3ea63bac7a4c8
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-05 19:00:48 +0900

    feat: MUI integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "../css/app.css";
+import { Box, Container, Stack, Typography, Button } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-  return <div>React</div>;
+  return (
+    <Container sx={{ background: "orange" }}>
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            Creating Burak Client Page
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={5}>
+            <Button variant="contained" color={"secondary"}>
+              Contained
+            </Button>
+          </RippleBadge>
+        </Box>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
