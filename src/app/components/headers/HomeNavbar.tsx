da85d98e71cdba388aa0e6bf00f6e49f54f4494a
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-10 20:36:43 +0900

    Publishing

---
+++ src/app/components/headers/HomeNavbar.tsx
@@ -0,0 +1,89 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+export function HomeNavbar() {
+  const authMember = null;
+  return (
+    <div className="home-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to="/">
+              <img
+                style={{ width: "125px", height: "30px" }}
+                src="/icons/burak.svg"
+              />
+            </NavLink>
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            minWidth={"700px"}
+            alignItems={"center"}
+          >
+            <Box className={"hover-line"}>
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink to="/products" activeClassName={"underline"}>
+                Products
+              </NavLink>
+            </Box>
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/orders" activeClassName={"underline"}>
+                  Orders
+                </NavLink>
+              </Box>
+            ) : null}
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page" activeClassName={"underline"}>
+                  My page
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"hover-line"}>
+              <NavLink to="/help" activeClassName={"underline"}>
+                Help
+              </NavLink>
+            </Box>
+            {!authMember ? (
+              <Box>
+                <Button
+                  variant="contained"
+                  style={{ background: "#3776cc", color: "##f8f8ff" }}
+                >
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                style={{ width: "50px", height: "50px", borderRadius: "24px" }}
+                src={"/icons/default-user.svg"}
+                aria-haspopup={"true"}
+              />
+            )}
+          </Stack>
+        </Stack>
+        <Stack className="header-frame">
+          <Stack className="detail">
+            <Box className="head-main-txt"> World's Most Delicious Cousine</Box>
+            <Box className="wel-txt">The Choice, not just a choice</Box>
+            <Box className="service-txt">24 hours service</Box>
+            <Box className="signup">
+              {!authMember ? (
+                <Button variant="contained" className="signup-button">
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Box className="logo-frame">
+            <div className="logo-img"></div>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
