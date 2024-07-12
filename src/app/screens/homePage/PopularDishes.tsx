052a4e2eec9d1c59bf99c5f763bd1606f1eb6b0d
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-12 21:22:27 +0900

    fix: Popular dishes

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -24,63 +24,67 @@ export default function PopularDishes() {
         <Stack className="popular-section">
           <Box className="category-title">Popular Dishes</Box>
           <Stack className="cards-frame">
-            {list.map((ele, index) => (
-              <CssVarsProvider key={index}>
-                <Card className="card">
-                  <CardCover>
-                    <img src={ele.imagePath} alt={ele.productName} />
-                  </CardCover>
-                  <CardCover className="card-cover" />
-                  <CardContent sx={{ justifyContent: "flex-end" }}>
-                    <Stack
-                      flexDirection={"row"}
-                      justifyContent={"space-between"}
+            {list?.length !== 0 ? (
+              list.map((ele, index) => (
+                <CssVarsProvider key={index}>
+                  <Card className="card">
+                    <CardCover>
+                      <img src={ele.imagePath} alt={ele.productName} />
+                    </CardCover>
+                    <CardCover className="card-cover" />
+                    <CardContent sx={{ justifyContent: "flex-end" }}>
+                      <Stack
+                        flexDirection={"row"}
+                        justifyContent={"space-between"}
+                      >
+                        <Typography
+                          level="h2"
+                          fontSize={"lg"}
+                          textColor={"#fff"}
+                          mb={1}
+                        >
+                          {ele.productName}
+                        </Typography>
+
+                        <Typography
+                          sx={{
+                            fontWeight: "md",
+                            color: "neutral.300",
+                            alignItems: "center",
+                            display: "flex",
+                          }}
+                        >
+                          7
+                          <VisibilityIcon
+                            sx={{ fontSize: 25, marginLeft: "5px" }}
+                          />
+                        </Typography>
+                      </Stack>
+                    </CardContent>
+
+                    <CardOverflow
+                      sx={{
+                        display: "flex",
+                        gap: 1.5,
+                        py: 1.5,
+                        px: "var(--Card-padding)",
+                        borderTop: "1px solid",
+                        height: "60px",
+                      }}
                     >
                       <Typography
-                        level="h2"
-                        fontSize={"lg"}
-                        textColor={"#fff"}
-                        mb={1}
+                        startDecorator={<DescriptionOutlinedIcon />}
+                        textColor={"neutral.300"}
                       >
-                        {ele.productName}
+                        This is delicious meal
                       </Typography>
-
-                      <Typography
-                        sx={{
-                          fontWeight: "md",
-                          color: "neutral.300",
-                          alignItems: "center",
-                          display: "flex",
-                        }}
-                      >
-                        {Math.floor(Math.random() * 600)}
-                        <VisibilityIcon
-                          sx={{ fontSize: 25, marginLeft: "5px" }}
-                        />
-                      </Typography>
-                    </Stack>
-                  </CardContent>
-
-                  <CardOverflow
-                    sx={{
-                      display: "flex",
-                      gap: 1.5,
-                      py: 1.5,
-                      px: "var(--Card-padding)",
-                      borderTop: "1px solid",
-                      height: "60px",
-                    }}
-                  >
-                    <Typography
-                      startDecorator={<DescriptionOutlinedIcon />}
-                      textColor={"neutral.300"}
-                    >
-                      This is delicious meal
-                    </Typography>
-                  </CardOverflow>
-                </Card>
-              </CssVarsProvider>
-            ))}
+                    </CardOverflow>
+                  </Card>
+                </CssVarsProvider>
+              ))
+            ) : (
+              <Box>No products are not available</Box>
+            )}
           </Stack>
         </Stack>
       </Container>
