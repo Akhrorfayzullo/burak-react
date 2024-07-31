4e788caa231ecc8c2b10226c5bfe4326afadc8de
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-31 20:19:54 +0900

    Redux

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,13 +1,69 @@
-import React from "react";
-import Statistics from "./Statistics";
+import React, { useEffect } from "react";
 import PopularDishes from "./PopularDishes";
+import Statistics from "./Statistics";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
+import { setPopularDishes } from "./slice";
+import { Product } from "../../../lib/types/product";
+import { createSelector, Dispatch } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { retrievePopularDishes } from "./selector";
 
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
 export function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
+  // Selector => Data
+  useEffect(() => {
+    // const result = [
+    //   {
+    //     _id: "jjgh7893043n49fh839",
+    //     productStatus: "PROCESS",
+    //     productCollection: "DISH",
+    //     productName: "Blabla",
+    //     productPrice: 56,
+    //     productCount: 2,
+    //     productSize: "NORMAL",
+    //     productVolume: 1,
+    //     productDesc: "product",
+    //   },
+    //   {
+    //     _id: "jjgh7893043n49fh839",
+    //     productStatus: "PROCESS",
+    //     productCollection: "DISH",
+    //     productName: "Blabla",
+    //     productPrice: 56,
+    //     productCount: 2,
+    //     productSize: "NORMAL",
+    //     productVolume: 1,
+    //     productDesc: "product",
+    //   },
+    //   {
+    //     _id: "jjgh7893043n49fh839",
+    //     productStatus: "PROCESS",
+    //     productCollection: "DISH",
+    //     productName: "Blabla",
+    //     productPrice: 56,
+    //     productCount: 2,
+    //     productSize: "NORMAL",
+    //     productVolume: 1,
+    //     productDesc: "product",
+    //   },
+    // ];
+    // @ts-ignore
+    // setPopularDishes(result);
+    //Backend =>Data
+    //Slice data => Store
+  }, []);
   return (
     <div className={"homepage"}>
       <Statistics />
