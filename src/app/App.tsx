5f1c8d5f3080217f6cfed7e227172721884a879c
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-08-07 20:19:36 +0900

    feat: Auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,10 +16,17 @@ import "../css/footer.css";
 import "../css/products.css";
 import HelpPage from "./screens/helpPage";
 import UseBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
 
 function App() {
   const location = useLocation();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = UseBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  //Handlers
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -59,6 +66,12 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
+      />
     </>
   );
 }
